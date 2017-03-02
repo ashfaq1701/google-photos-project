bab7d30163040048d43de55120e5e4baddacf536
root <ashfaq1701@gmail.com>
2017-03-02 23:57:14 +0530

    changed user identity method

--- index.php
+++ index.php
@@ -11,12 +11,15 @@
 	$client->addScope('https://www.googleapis.com/auth/plus.me');
 	$client->addScope('https://www.googleapis.com/auth/userinfo.profile');
 	$client->addScope('https://picasaweb.google.com/data/');
+	$client->addScope('https://www.googleapis.com/auth/plus.business.manage');
 	if (isset($_SESSION['access_token']) && $_SESSION['access_token']) 
 	{
 		$client->setAccessToken($_SESSION['access_token']);
 		$token = $_SESSION['access_token']['access_token'];
-		$albums = get_all_albums($token);
-		print_r($albums);
+		//$albums = get_all_albums($token);
+		//print_r($albums);
+		$accounts = get_all_accounts($token);
+		print_r($accounts);
 	}
 	else {
 		$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
--- lib/functions.php
+++ lib/functions.php
@@ -5,10 +5,16 @@ use GuzzleHttp\Client;
 function get_all_albums($token)
 {
 	$url = 'https://picasaweb.google.com/data/feed/api/user/default?access_token='.$token;
-	echo $url.'<br/><br/>';
 	$client = new Client();
 	$res = $client->request('GET', $url);
 	return $res->getBody()->getContents();
+}
+
+function get_all_accounts($token)
+{
+	$url = 'https://mybusiness.googleapis.com/v3/accounts?access_token='.$token;
+	$client = new Client();
+	return $res->getBody()->getClients();
 }
 
 ?>
