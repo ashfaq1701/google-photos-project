15769bec525dbb7c55526b75c1f5e4010a3b32a9
root <ashfaq1701@gmail.com>
2017-03-02 12:35:54 +0530

    changed user identity method

--- lib/functions.php
+++ lib/functions.php
@@ -4,8 +4,7 @@ use GuzzleHttp\Client;
 
 function get_all_albums($userId, $token)
 {
-	$url = 'https://picasaweb.google.com/data/feed/api/user/'.$userId;
-	echo $userId;
+	$url = 'https://picasaweb.google.com/data/feed/api/user/default';
 	$client = new Client();
 	$res = $client->request('GET', $url.'?access_token='.$token);
 	return $res->getBody()->getContents();
