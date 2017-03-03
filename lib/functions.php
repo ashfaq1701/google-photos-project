1b58620641735577b2facc4890ada5393cd1fa94
root <ashfaq1701@gmail.com>
2017-03-03 21:38:01 +0530

    changed user identity method

--- lib/functions.php
+++ lib/functions.php
@@ -10,7 +10,7 @@ function get_all_albums($userId, $token)
 	$albumsString = $res->getBody()->getContents();
 	$albumXml = simplexml_load_string($albumsString) or die("Error: Cannot create object");
 	$categories = $albumXml->category[0];//->subtitle->entry->category;
-	echo getClass($categories);
+	echo get_class($categories);
 	/*foreach ($categories as $category)
 	{
 		$currentEntries = $category->summary->entry;
